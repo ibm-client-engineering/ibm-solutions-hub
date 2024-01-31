406e5851a840e2196f18d55bd00c5e10c7d16406
Girish Tiwari <gt@Girishs-MacBook-Pro.local>
2024-01-31 13:11:02 -0500

    search on name and description functional

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,11 @@
 'use client';
 
-import { Link, Grid, Column, Search} from "@carbon/react";
+import { Link, Grid, Column, Search, ClickableTile, Tag} from "@carbon/react";
 import React, { useEffect, useState } from "react";
 import ProjectsTiles from "./ProjectsTiles";
 import Loading from "@carbon/react/lib/components/Loading";
+import { setIn } from "immutable";
+import { UserServiceDesk, Launch } from "@carbon/icons-react";
 
 const { Octokit, App } = require("@octokit/core");
 
@@ -73,6 +75,23 @@ function ProjectsPage() {
   const [repoData, setRepoData] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  let searchProjects = (e) => {
+    const inputText = e.target.value.toLowerCase();
+
+    nodes.forEach((node) => {
+      const isVisible =
+        node.name.toLowerCase().includes(inputText) || 
+        node.description.toLowerCase().includes(inputText);
+
+      if (isVisible)
+        document.getElementById(node.name).style.display = "block";
+      else
+        document.getElementById(node.name).style.display = "none";
+    })
+
+    setRepoData(nodes);
+  }
+
   useEffect(() => {
     async function getGitHubRepos() {
       setLoading(true);
@@ -138,7 +157,8 @@ function ProjectsPage() {
       <Grid fullWidth narrow>
         <Column className="banner-container" lg={16} md={8} sm={4}>
             <Column className="banner-title-container" lg={8} md={4} sm={2}>
-              <h1 className="banner-title">Projects</h1>
+              <p className="banner-title">Projects</p>
+              <Search className="banner-search" size="lg" placeholder="Find a project" labelText="Search" closeButtonLabelText="Clear search input"/>
             </Column>
             <Column className="banner-image-container" lg={8} md={4} sm={2}>
             </Column>
@@ -148,22 +168,38 @@ function ProjectsPage() {
         </Column>
       </Grid>
     );
-  else
-    return (
-      <Grid fullWidth narrow>
-        <Column className="banner-container" lg={16} md={8} sm={4}>
-            <Column className="banner-title-container" lg={8} md={4} sm={2}>
-              <p className="banner-title">Projects</p>
-              <Search className="banner-search" size="lg" placeholder="Find a project" labelText="Search" closeButtonLabelText="Clear search input"/>
-            </Column>
-            <Column className="banner-image-container" lg={8} md={4} sm={2}>
-            </Column>
-        </Column>
-        <Column lg={16} md={8} sm={2} className="repoTiles">
-          <ProjectsTiles data={repoData}/>
-        </Column>
-      </Grid>
-    );
+  else {
+    try {
+      return (
+        <Grid fullWidth narrow>
+          <Column className="banner-container" lg={16} md={8} sm={4}>
+              <Column className="banner-title-container" lg={8} md={4} sm={2}>
+                <p className="banner-title">Projects</p>
+                <Search className="banner-search" size="lg" placeholder="Find a project" labelText="Search" closeButtonLabelText="Clear search input" onChange={searchProjects}/>
+              </Column>
+              <Column className="banner-image-container" lg={8} md={4} sm={2}>
+              </Column>
+          </Column>
+          <Column lg={16} md={8} sm={2} className="repoTiles">
+            {repoData.map((item, index) => (
+              <ClickableTile className="projectTile" id={item.name} key={index} href={item.homepageUrl} target="_blank" rel="noopener noreferrer" renderIcon={Launch}>
+                  <h6 className="projectTile__title">{item.name}</h6>
+                  <p3 className="projectTile__description">{item.description}</p3>
+                  {item.repositoryTopics.map((topic, index) => (
+                      <Tag className="projectTile__topics" key={index}>
+                          {topic}
+                      </Tag>
+                  ))}
+              </ClickableTile>
+            ))}
+          </Column>
+        </Grid>
+      );
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
 }
 
 export default ProjectsPage;
