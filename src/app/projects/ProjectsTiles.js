8b227d1d7ee2de76ae849435367c8fff24f065b9
Sahaj Zalavadia <Sahaj.Zalavadia@ibm.com>
2024-05-21 15:51:21 -0400

    added key iterator

--- src/app/projects/ProjectsTiles.js
+++ src/app/projects/ProjectsTiles.js
@@ -10,12 +10,12 @@ import {
 const ProjectsTiles = ({ data }) => {
   return (
     <>
-      {data.map((repo) => (
-        <ClickableTile id={repo.name} className="projectTile" href={repo.homepageUrl} target="_blank" rel="noopener noreferrer" renderIcon={Launch}>
+      {data.map((repo, index) => (
+        <ClickableTile id={repo.name} className="projectTile" key={index} href={repo.homepageUrl} target="_blank" rel="noopener noreferrer" renderIcon={Launch}>
           <h6 className="projectTile__title">{repo.title}</h6>
           <p3 className="projectTile__description">{repo.description}</p3>
-          {repo.repositoryTopics.nodes.map((nodes) => (
-                    <Tag className="projectTile__topics">
+          {repo.repositoryTopics.nodes.map((nodes, index) => (
+                    <Tag className="projectTile__topics" key={index}>
                         {nodes.topic.name}
                     </Tag>
                 ))}
