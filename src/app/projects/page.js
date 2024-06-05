8dd140ad7af013b7cf2de3152c0c0a7761963974
Samuel-L <Samuel.L@ibm.com>
2024-06-05 16:13:31 -0400

    fixed issue where the projects wouldnt reset after removing filters

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -7,8 +7,10 @@ import ProjectsTiles from "./ProjectsTiles";
 import data from "../../../repoData.json"
 
 var repoData = data.organization.repositories.nodes.filter(repo => repo.publish === 'True');
+var repoFiltered = new Map() //for checking if the repo falls under the current search or tag filters
 var tags = new Set()
 repoData.forEach((node) => {
+  repoFiltered.set(node.name, {"tag": true, "search": true})
   node.repositoryTopics.nodes.forEach((tag) => {
     tags.add(tag.topic.name)
   })
@@ -17,6 +19,17 @@ const tagsArray = Array.from(tags)
 
 
 function ProjectsPage() {
+
+  let renderProjects = () => {
+    repoData.forEach((node) => {
+      if (document.getElementById(node.name)) {
+        if (repoFiltered.get(node.name).tag && repoFiltered.get(node.name).search)
+          document.getElementById(node.name).style.display = "block";
+        else
+          document.getElementById(node.name).style.display = "none";
+      }
+    })
+  }
 
   let searchProjects = (e) => {
     const inputText = e.target.value.toLowerCase();
@@ -38,19 +51,18 @@ function ProjectsPage() {
         node.title && node.title.toLowerCase().includes(inputText);
 
       if (document.getElementById(node.name)) {
-        if (isVisible)
-          document.getElementById(node.name).style.display = "block";
-        else
-          document.getElementById(node.name).style.display = "none";
-      }
-    })
+        var temp = repoFiltered.get(node.name)
+        temp.search = isVisible
+        repoFiltered.set(node.name, temp)
+    }})
+    renderProjects()
   }
 
   let filterProjects = (e) => {
     const inputTags = e.selectedItems;
     repoData.forEach((node) => {
       console.log(node)
-      var inRepo = false
+      var inRepo = inputTags.length == 0
       node.repositoryTopics.nodes.forEach((tag) => {
         if (inputTags.includes(tag.topic.name.toLowerCase())) {
           inRepo = true
@@ -58,10 +70,12 @@ function ProjectsPage() {
       })
 
       if (document.getElementById(node.name)) {
-        if (!inRepo)
-          document.getElementById(node.name).style.display = "none";
+        var temp = repoFiltered.get(node.name)
+        temp.tag = inRepo
+        repoFiltered.set(node.name, temp)
       }
     })
+    renderProjects()
   }
 
 
