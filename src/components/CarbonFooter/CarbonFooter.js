4d2344c11867e675db3cb8688960d6378a46df9f
Dhanvi Shivadevuni <Dhanvi.Shivadevuni@ibm.com>
2024-06-06 14:33:39 -0400

    Floating Footer Fix

--- src/components/CarbonFooter/CarbonFooter.js
+++ src/components/CarbonFooter/CarbonFooter.js
@@ -1,6 +1,22 @@
 import C4DFooterContainer from '@carbon/ibmdotcom-web-components/es/components-react/footer/footer-container';
 
+// const CarbonFooter = () => (
+//   <div style={{ marginTop: 'auto', width: '100%' }}>
+//     <C4DFooterContainer size="micro" />
+//   </div>
+// );
+
 const CarbonFooter = () => (
-  <C4DFooterContainer size="short" />
-)
+  <div style={{ marginTop: 'auto', width: '100%' }}>
+    <style>{`
+      .c4d--locale-btn {
+        display: none;
+      }
+    `}</style>
+    <C4DFooterContainer size="micro" />
+  </div>
+);
+
 export default CarbonFooter;
+
+// export default CarbonFooter;
