46d8613e5d07fb87cc28ba59a4f7e8a32761a240
Samuel-L <Samuel.L@ibm.com>
2024-10-30 11:34:52 -0400

    Added technology to project tile

--- src/app/projects/ProjectsTiles.js
+++ src/app/projects/ProjectsTiles.js
@@ -14,6 +14,13 @@ const ProjectsTiles = ({ data }) => {
         <ClickableTile id={repo.name} className="projectTile" key={index} href={repo.homepageUrl} target="_blank" rel="noopener noreferrer" renderIcon={Launch}>
           <h6 className="projectTile__title">{repo.title}</h6>
           <p3 className="projectTile__description">{repo.description}</p3>
+          { Object.hasOwn(repo, "technology") ? 
+          repo.technology.map((tech, index) => (
+            <Tag className="projectTile__techs" type="blue" key={index}>
+              {tech}
+            </Tag>
+          )) : <></>}
+          <br></br>
           {repo.repositoryTopics.nodes.map((nodes, index) => (
             <Tag className="projectTile__topics" key={index}>
               {nodes.topic.name}
