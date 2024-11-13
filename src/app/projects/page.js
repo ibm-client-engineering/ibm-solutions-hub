04f55ef9cac36c972fd88074714b0cefefd8f228
Samuel-L <Samuel.L@ibm.com>
2024-11-13 16:21:23 -0500

    Updated project page to the new layout

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,31 +9,34 @@ const repoData = data.organization.repositories.nodes.filter(repo => repo.publis
 // For checking if the repo falls under the current search or topic filters
 // It maps the repo name to an object that describes if it falls under the search filter and/or the topic .ilter
 const repoFiltered = new Map(); 
-const topics = new Set();
+const industries = new Set();
 const techs = new Set();
 repoData.forEach((node) => {
-  repoFiltered.set(node.name, { "topic": true, "search": true, "tech": true });
-  node.repositoryTopics.nodes.forEach((topic) => {
-    topics.add(topic.topic.name);
-  });
+  repoFiltered.set(node.name, { "industry": true, "search": true, "tech": true });
+  industries.add(node.industry);
   //if the object has a technology field, add it to the techs set
   Object.hasOwn(node, "technology") ? node.technology.forEach((tech) => {techs.add(tech);}) : {};
 });
 const techsArray = Array.from(techs);
+const industriesArray = Array.from(industries);
 
 // Parse URL parameters outside the component
 const urlParams = typeof window !== 'undefined' ? new URLSearchParams(window.location.search) : null;
-const tech = urlParams ? urlParams.get('tech') : null;
+const industry = urlParams ? urlParams.get('industry') : null;
+const initialSelectedIndustries = industry ? [industry] : [];
+const tech = urlParams ? urlParams.get('industry') : null;
 const initialSelectedTechs = tech ? [tech] : [];
+
 
 function ProjectsPage() {
   const [selectedTechs, setSelectedTechs] = useState(initialSelectedTechs);
+  const [selectedIndustries, setSelectedIndustries] = useState(initialSelectedIndustries);
 
   // Define the renderProjects function, which updates the page based on the user inputted filters
   const renderProjects = () => {
     repoData.forEach((node) => {
       if (document.getElementById(node.name)) {
-        if (repoFiltered.get(node.name).topic && repoFiltered.get(node.name).search && repoFiltered.get(node.name).tech) {
+        if (repoFiltered.get(node.name).industry && repoFiltered.get(node.name).search && repoFiltered.get(node.name).tech) {
           document.getElementById(node.name).style.display = "block";
         } else {
           document.getElementById(node.name).style.display = "none";
@@ -81,7 +84,7 @@ function ProjectsPage() {
       const inputTechs = e.selectedItems;
       setSelectedTechs(inputTechs);
       repoData.forEach((node) => {
-        const nodeTechs = node.technology
+        const nodeTechs = node.technology;
         //Need to check that the repo has a technology field, and if it does need to check if the selected tech(s) is in that array
         //in the case where the repo has no techs listed, still needs to be true if there are no selected techs
         const inRepo =  inputTechs.every((tech) => Object.hasOwn(node, "technology") ? nodeTechs.includes(tech) : inputTechs.length == 0);
@@ -95,32 +98,75 @@ function ProjectsPage() {
       renderProjects();
     };
 
+    //callback function for the industry filter, filters the projects by the industries selected
+    const filterProjectsIndustry = (e) => {
+      const inputIndustries = e.selectedItems;
+      setSelectedIndustries(inputIndustries);
+      repoData.forEach((node) => {
+        const nodeIndustry = node.industry
+        //Need to check that the repo has an industry field (it should), and if it does need to check if the selected industry/industries match
+        //in the case where the repo has no industry listed, still needs to be true if there are no selected techs
+        const inRepo =  inputIndustries.some((industry) => Object.hasOwn(node, "industry") ? nodeIndustry === industry : inputIndustries.length == 0);
+
+        if (document.getElementById(node.name)) {
+          const temp = repoFiltered.get(node.name);
+          temp.industry = inputIndustries.length > 0 ? inRepo : true;
+          repoFiltered.set(node.name, temp);
+        }
+      });
+      renderProjects();
+    };
+
   return (
     <Grid fullWidth narrow>
-      <Column className="banner-container" lg={16} md={8} sm={4}>
-        <Column className="banner-title-container" lg={8} md={4} sm={2}>
+      <Column className="banner-container" lg={16} md={8} sm={4}> {/*lg={16} md={8} sm={4}*/}
+        <Column className="banner-title-container" lg={8} md={4} sm={2}> {/*lg={8} md={4} sm={2}*/}
           <p className="banner-title">Projects</p>
-          <Row className='search-row'>
-            <Search className="banner-search" size="lg" placeholder="Search" labelText="Search" closeButtonLabelText="Clear search input" onChange={searchProjects} />
-            <FilterableMultiSelect 
-              id="carbon-multiselect" 
-              className="filter-search" 
-              size="lg" 
-              placeholder="Technology" 
-              items={techsArray} 
-              itemToString={item => item ? item : ''} 
-              selectionFeedback="top-after-reopen" 
-              onChange={filterProjectsTech} 
-              initialSelectedItems={selectedTechs} // Set selected items based on state
+        </Column>
+        <Column className="banner-image-container" lg={8} md={4} sm={2}></Column>
+      </Column>
+      <Column className='search-row' lg={16} md={8} sm={4}>
+        <Column className="filters__col">
+          <Column>
+          <FilterableMultiSelect 
+            id="carbon-multiselect" 
+            className="filter-search" 
+            size="lg" 
+            placeholder="Industry" 
+            items={industriesArray} 
+            itemToString={item => item ? item : ''} 
+            selectionFeedback="top-after-reopen" 
+            onChange={filterProjectsIndustry} 
+            initialSelectedItems={selectedIndustries} // Set selected items based on state
+          />
+          </Column>
+          <Column>
+          <FilterableMultiSelect 
+            id="carbon-multiselect" 
+            className="filter-search" 
+            size="lg" 
+            placeholder="Technology" 
+            items={techsArray} 
+            itemToString={item => item ? item : ''} 
+            selectionFeedback="top-after-reopen" 
+            onChange={filterProjectsTech} 
+            initialSelectedItems={selectedTechs} // Set selected items based on state
             />
-          </Row>
+          </Column>
         </Column>
-        <Column className="banner-image-container" lg={8} md={4} sm={2}>
+        
+        <Column className="search_projects__col">
+          <Column>
+            <Search className="banner-search" size="lg" placeholder="Search" labelText="Search" closeButtonLabelText="Clear search input" onChange={searchProjects} />
+          </Column>
+          <Column className="repoTiles">
+            <ProjectsTiles data={repoData}></ProjectsTiles>
+          </Column>
+
         </Column>
       </Column>
-      <Column lg={16} md={8} sm={2} className="repoTiles">
-        <ProjectsTiles data={repoData}></ProjectsTiles>
-      </Column>
+        
+
     </Grid>
   );
 }
