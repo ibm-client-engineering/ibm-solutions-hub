38ad701764347e527e020551b09a47703b12d65f
Samuel-L <Samuel.L@ibm.com>
2024-11-13 12:01:27 -0500

    Adjusted tags on repo tiles

--- src/app/projects/ProjectsTiles.js
+++ src/app/projects/ProjectsTiles.js
@@ -1,10 +1,11 @@
 'use client';
 import React from 'react';
-import { Launch, ToolKit } from '@carbon/icons-react';
+import { Launch, ToolKit, Enterprise } from '@carbon/icons-react';
 import {
   ClickableTile,
   Tag,
   Row,
+  Column
 } from "@carbon/react";
 
 const ProjectsTiles = ({ data }) => {
@@ -16,13 +17,17 @@ const ProjectsTiles = ({ data }) => {
           <p3 className="projectTile__industry">{repo.industry}</p3>
           <p3 className="projectTile__description">{repo.description}</p3>
           <Row className="project__row">
-          <ToolKit className="toolkit_icon"/>
-          { Object.hasOwn(repo, "technology") ? 
-          repo.technology.map((tech, index) => (
-            <Tag className="projectTile__techs" type="blue" key={index}>
-              {tech}
-            </Tag>
-          )) : <></>}
+            <Column className='icon__column' lg={1}><Enterprise className="enterprise_icon"/></Column>
+            <Column className='tag__column'><Tag className="projectTile__industry" type="cyan" key={index}>{repo.industry}</Tag></Column>
+          </Row>
+          <Row className="project__row">
+          <Column className='icon__column' lg={1}><ToolKit className="toolkit_icon"/></Column>
+          <Column className='tag__column'>{ Object.hasOwn(repo, "technology") ? 
+            repo.technology.map((tech, index) => (
+              <Tag className="projectTile__techs" type="blue" key={index}>
+                {tech}
+              </Tag>
+            )) : <></>}</Column>
           </Row>
         </ClickableTile>
       ))}
