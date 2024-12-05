81be84274b23420da3e858c139131bbffa3ab3ad
Samuel-L <Samuel.L@ibm.com>
2024-12-05 09:29:45 -0800

    started update to mission page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,22 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.scss';
 import {Row, Column, FlexGrid, TextInput, TextArea, Button} from '@carbon/react';
-import assistant from './assistant.png';
+
 
 const ContactUs = () => {
+
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const submit = (e) => {
+    const output = `fn: ${firstName}, ln: ${lastName}, em: ${email}, mg: ${message}`;
+    console.log(output);
+  }
+
   return (
     <FlexGrid className="contact__grid">
       <Row className='contact_banner__row' lg={6}>
@@ -20,24 +31,24 @@ const ContactUs = () => {
         <Column className='form__column'>
           <Row className='name__row form__row'>
             <Column className='firstname__column'>
-              <TextInput id="firstname-input" type="text" labelText="First Name" />
+              <TextInput id="firstname-input" type="text" labelText="First Name" value={firstName}/>
             </Column>
             <Column className='lastname__column'>
-              <TextInput id="lastname-input" type="text" labelText="Last Name" />
+              <TextInput id="lastname-input" type="text" labelText="Last Name"  value={lastName}/>
             </Column>
           </Row>
           <Row className='email__row form__row'>
             <Column className='email__column'>
-              <TextInput id="email-input" type="text" labelText="Email" />
+              <TextInput id="email-input" type="text" labelText="Email"  value={email}/>
             </Column>
           </Row>
           <Row className='message__row form__row'>
             <Column className='message__column'>
-              <TextArea id="message-input" type="text" labelText="Message"/>
+              <TextArea id="message-input" type="text" labelText="Message"  value={message}/>
             </Column>
           </Row>
           <Row className='submit__row form__row'>
-            <Button className='submit__button'>Submit</Button>
+            <Button className='submit__button' onClick={submit}>Submit</Button>
           </Row>
         </Column>
       </Row>
--- src/app/mission/page.js
+++ src/app/mission/page.js
@@ -2,30 +2,24 @@
 
 import React from 'react';
 import './mission.scss'; 
+import {Row, Column, FlexGrid} from '@carbon/react';
 
 const MissionPage = () => {
   return (
-    <div className="mission-page">
-      <h1>Our Mission</h1>
-      <p>
-        The Solutions Hub documents best practices for creating technical solutions at IBM Client Engineering. 
-        It represents IBM&apos;s commitment to transparency and collaboration, showcasing real-life customer 
-        experiences to encourage the adoption of IBM Technology. This site allows IBMers, clients, and 
-        interested parties to participate, provide feedback, and benefit from collective knowledge.
-        IBM Client Engineering collaborates with clients to co-create and innovate, utilizing IBM technology 
-        and methodologies to address business challenges. 
-      </p>
-      <p>
-        Client Engineering is a no-cost investment for IBM&apos;s 
-        clients. Connect with us today!
-      </p>
-      <a href="https://www.ibm.com/services/client-engineering" target="_blank" rel="noopener noreferrer">
-        Learn more about Client Engineering →
-      </a>
-      <div className="image-container">
-        <img src="https://www.ibm.com/content/dam/connectedassets-adobe-cms/worldwide-content/creative-assets/ibs/ul/g/66/9e/20230607_ibm_thomasprior_albany_4437.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg" alt="Team collaboration" />
-      </div>
-    </div>
+    <FlexGrid className="mission__grid">
+      <Row className='mission_banner__row' lg={6}>
+        <Column className="title__column" lg={8}>
+          <h1>Contact us</h1>
+        </Column>
+        <Column className='mission_img__container' lg={8}>
+        </Column>
+        
+      </Row>
+      <Row className="body__row" lg={10}>
+        <Column className='body__column'>
+        </Column>
+      </Row>
+    </FlexGrid>
   );
 };
 
