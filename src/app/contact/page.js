b1d9c59b41f8c73b34139e5c768ba79cc6852aff
Samuel-L <Samuel.L@ibm.com>
2024-12-09 16:03:39 -0500

    Added contact us page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,49 +7,19 @@ import {Row, Column, FlexGrid, TextInput, TextArea, Button} from '@carbon/react'
 
 const ContactUs = () => {
 
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [email, setEmail] = useState();
-  const [message, setMessage] = useState();
-
-  const submit = (e) => {
-    const output = `fn: ${firstName}, ln: ${lastName}, em: ${email}, mg: ${message}`;
-    console.log(output);
-  }
-
   return (
     <FlexGrid className="contact__grid">
       <Row className='contact_banner__row' lg={6}>
         <Column className="title__column" lg={8}>
-          <h1>Contact us</h1>
+          <h1>Contact Us</h1>
         </Column>
         <Column className='assistant_img__container' lg={8}>
         </Column>
         
       </Row>
       <Row className="body__row" lg={10}>
-        <Column className='form__column'>
-          <Row className='name__row form__row'>
-            <Column className='firstname__column'>
-              <TextInput id="firstname-input" type="text" labelText="First Name" value={firstName}/>
-            </Column>
-            <Column className='lastname__column'>
-              <TextInput id="lastname-input" type="text" labelText="Last Name"  value={lastName}/>
-            </Column>
-          </Row>
-          <Row className='email__row form__row'>
-            <Column className='email__column'>
-              <TextInput id="email-input" type="text" labelText="Email"  value={email}/>
-            </Column>
-          </Row>
-          <Row className='message__row form__row'>
-            <Column className='message__column'>
-              <TextArea id="message-input" type="text" labelText="Message"  value={message}/>
-            </Column>
-          </Row>
-          <Row className='submit__row form__row'>
-            <Button className='submit__button' onClick={submit}>Submit</Button>
-          </Row>
+        <Column className='form__column' lg={6}>
+        <iframe src="https://forms.monday.com/forms/embed/4414a889642bf5258f8c63f7155748cb?r=use1" width="1400" height="1200" align="center" className="monday__form"></iframe>
         </Column>
       </Row>
     </FlexGrid>
